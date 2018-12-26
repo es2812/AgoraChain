efd95a72c56ffde48d083a1145fd090dd63dea09
es2812 <es2812@hotmail.com>
2018-12-26 11:21:57 +0100

    Changed scripts to assume .bna file is already created

--- composer-env/agora-network/dist/deploy.sh
+++ composer-env/agora-network/dist/deploy.sh
@@ -2,7 +2,7 @@
 if [[ -z $1 ]]; then
     echo usage: deploy_and_test.sh VERSION
 else
-    ./createFile.sh $1
+    #./createFile.sh $1
     ./start.sh $1
     ./initialization.sh
 fi
--- composer-env/agora-network/dist/start.sh
+++ composer-env/agora-network/dist/start.sh
@@ -14,11 +14,18 @@ else
     
     composer card delete -c admin@agora-network
     composer card delete -c PeerAdmin@hlfv1
-    ~/fabric-dev-servers/startFabric.sh
-    ~/fabric-dev-servers/createPeerAdminCard.sh
-    echo Installing and starting network v.$1... 
-    composer network install --card PeerAdmin@hlfv1 --archiveFile agora-network@$1.bna
-    composer network start --networkName agora-network --networkVersion $1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
-    composer card import --file networkadmin.card
-    composer network ping --card admin@agora-network
+    if ~/fabric-dev-servers/startFabric.sh
+    then 
+        if  ~/fabric-dev-servers/createPeerAdminCard.sh
+        then echo Installing and starting network v.$1... 
+            if composer network install --card PeerAdmin@hlfv1 --archiveFile agora-network@$1.bna
+            then 
+                if composer network start --networkName agora-network --networkVersion $1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+                then
+                composer card import --file networkadmin.card
+                composer network ping --card admin@agora-network
+                fi
+            fi
+        fi
+    fi
 fi
