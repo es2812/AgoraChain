d7c08c44054fabbe2c74becc7223119290a67f99
es2812 <es2812@hotmail.com>
2018-12-30 20:26:46 +0100

    Code cleaned and form validation implemented on TX_AddRestriction

--- angular-api/AgoraChain/src/app/TX_AddRestriction/TX_AddRestriction.component.ts
+++ angular-api/AgoraChain/src/app/TX_AddRestriction/TX_AddRestriction.component.ts
@@ -37,22 +37,13 @@ export class TX_AddRestrictionComponent implements OnInit {
   private errorMessage;
   private uniqueID;
 
-  restrictionID = new FormControl({value:'', disabled:true}, Validators.required);
   category = new FormControl('', Validators.required);
   choice = new FormControl('', Validators.required);
-  trustee = new FormControl({value:'', disabled:true}, Validators.required);
-  transactionId = new FormControl('', Validators.required);
-  timestamp = new FormControl('', Validators.required);
-
 
   constructor(private serviceTX_AddRestriction: TX_AddRestrictionService, private serviceIdentity:IdentityService, private serviceRestrictions: DataService<Restriction>, private spinnerService: NgxSpinnerService, fb: FormBuilder) {
     this.myForm = fb.group({
-      restrictionID: this.restrictionID,
       category: this.category,
-      choice: this.choice,
-      trustee: this.trustee,
-      transactionId: this.transactionId,
-      timestamp: this.timestamp
+      choice: this.choice
     });
   };
 
@@ -75,6 +66,7 @@ export class TX_AddRestrictionComponent implements OnInit {
       } else {
         this.errorMessage = error;
       }
+      this.spinnerService.hide()
     });
   }
 
@@ -101,67 +93,27 @@ export class TX_AddRestrictionComponent implements OnInit {
       } else {
         this.errorMessage = error;
       }
+      this.spinnerService.hide()
     });
-  }
-
-	/**
-   * Event handler for changing the checked state of a checkbox (handles array enumeration values)
-   * @param {String} name - the name of the transaction field to update
-   * @param {any} value - the enumeration value for which to toggle the checked state
-   */
-  changeArrayValue(name: string, value: any): void {
-    const index = this[name].value.indexOf(value);
-    if (index === -1) {
-      this[name].value.push(value);
-    } else {
-      this[name].value.splice(index, 1);
-    }
-  }
-
-	/**
-	 * Checkbox helper, determining whether an enumeration value should be selected or not (for array enumeration values
-   * only). This is used for checkboxes in the transaction updateDialog.
-   * @param {String} name - the name of the transaction field to check
-   * @param {any} value - the enumeration value to check for
-   * @return {Boolean} whether the specified transaction field contains the provided value
-   */
-  hasArrayValue(name: string, value: any): boolean {
-    return this[name].value.indexOf(value) !== -1;
   }
 
   addTransaction(form: any): Promise<any> {
     this.spinnerService.show();
     this.Transaction = {
       $class: 'org.agora.net.TX_AddRestriction',
-      'restrictionID': this.restrictionID.value,
-      'category': this.category.value,
-      'choice': this.choice.value,
-      'trustee': this.trustee.value,
-      'transactionId': this.transactionId.value,
-      'timestamp': this.timestamp.value
-    };
-
-    this.myForm.setValue({
-      'restrictionID': null,
-      'category': null,
-      'choice': null,
-      'trustee': null,
+      'restrictionID': this.uniqueID,
+      'category': form.get('category').value,
+      'choice': form.get('choice').value,
+      'trustee': this.currentTrustee,
       'transactionId': null,
       'timestamp': null
-    });
+    };
 
     return this.serviceTX_AddRestriction.addTransaction(this.Transaction)
     .toPromise()
     .then(() => {
       this.errorMessage = null;
-      this.myForm.setValue({
-        'restrictionID': null,
-        'category': null,
-        'choice': null,
-        'trustee': null,
-        'transactionId': null,
-        'timestamp': null
-      });
+      this.resetForm();
       this.spinnerService.hide()
     })
     .catch((error) => {
@@ -170,85 +122,12 @@ export class TX_AddRestrictionComponent implements OnInit {
       } else {
         this.errorMessage = error;
       }
-    });
-  }
-  setId(id: any): void {
-    this.currentId = id;
-  }
 
-  getForm(id: any): Promise<any> {
-
-    return this.serviceTX_AddRestriction.getTransaction(id)
-    .toPromise()
-    .then((result) => {
-      this.errorMessage = null;
-      const formObject = {
-        'restrictionID': null,
-        'category': null,
-        'choice': null,
-        'trustee': null,
-        'transactionId': null,
-        'timestamp': null
-      };
-
-      if (result.restrictionID) {
-        formObject.restrictionID = result.restrictionID;
-      } else {
-        formObject.restrictionID = null;
-      }
-
-      if (result.category) {
-        formObject.category = result.category;
-      } else {
-        formObject.category = null;
-      }
-
-      if (result.choice) {
-        formObject.choice = result.choice;
-      } else {
-        formObject.choice = null;
-      }
-
-      if (result.trustee) {
-        formObject.trustee = result.trustee;
-      } else {
-        formObject.trustee = null;
-      }
-
-      if (result.transactionId) {
-        formObject.transactionId = result.transactionId;
-      } else {
-        formObject.transactionId = null;
-      }
-
-      if (result.timestamp) {
-        formObject.timestamp = result.timestamp;
-      } else {
-        formObject.timestamp = null;
-      }
-
-      this.myForm.setValue(formObject);
-
-    })
-    .catch((error) => {
-      if (error === 'Server error') {
-        this.errorMessage = 'Could not connect to REST server. Please check your configuration details';
-      } else if (error === '404 - Not Found') {
-      this.errorMessage = '404 - Could not find API route. Please check your available APIs.';
-      } else {
-        this.errorMessage = error;
-      }
+      this.spinnerService.hide()
     });
   }
 
   resetForm(): void {
-    this.myForm.setValue({
-      'restrictionID': this.uniqueID,
-      'category': null,
-      'choice': null,
-      'trustee': this.currentTrustee,
-      'transactionId': null,
-      'timestamp': null
-    });
+    this.myForm.reset();
   }
 }
